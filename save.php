3eb115fee9cde1b275b19ceee88c4b1c331739af
vetsinen <vetsinen@gmail.com>
2017-11-02 15:33:23 +0200

    saves images locally

--- save.php
+++ save.php
@@ -1,6 +1,6 @@
 <?php
-//$postdata = file_get_contents("php://input");
-if (isset($postdata)) {
+$imageData = file_get_contents("php://input");
+if (isset($imageData)) {
     error_log('with data now');
     // Get the data
 //    $imageData = $postdata;
@@ -13,10 +13,12 @@ if (isset($postdata)) {
     // Save file. This example uses a hard coded filename for testing,
     // but a real application can specify filename in POST variable
     $fn = str_pad(rand(1,PHP_INT_MAX), 19, '0', STR_PAD_LEFT);
-    $fp = fopen($fn.'.png', 'wb');
+    $fullpath =__DIR__ .'/'.$fn.'.png';
+    error_log($fullpath);
+    $fp = fopen($fullpath, 'wb');
     fwrite($fp, $unencodedData);
     fclose($fp);
-    echo $fp;
+    echo $fn;
 } else {
     error_log('no data');
 }
--- tst.php
+++ tst.php
@@ -1,6 +1,13 @@
 <?php
+$fn = str_pad(rand(1,PHP_INT_MAX), 19, '0', STR_PAD_LEFT);
+$fullpath =__DIR__ .'/'.$fn.'.txt';
+echo $fullpath;
+$fp = fopen($fullpath, 'wb');
+
+fwrite($fp, '123456789');
+fclose($fp);
 //echo strlen( strval(PHP_INT_MAX)); -> 19
-for ($i=1;$i<100;$i++){
-    $f = str_pad(rand(1,PHP_INT_MAX), 19, '0', STR_PAD_LEFT);
-    echo "\n".$f;
-}
+//for ($i=1;$i<100;$i++){
+//    $f = str_pad(rand(1,PHP_INT_MAX), 19, '0', STR_PAD_LEFT);
+//    echo "\n".$f;
+//}
